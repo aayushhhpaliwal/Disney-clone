70b78c734a8e41758f51721dc39759b7da931b5d
aayushhhpaliwal <aayushpaliwal653@gmail.com>
2022-11-24 12:48:52 +0530

    checking auth

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Header />
+        <Header />  
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/home' element={<Home />}/>
